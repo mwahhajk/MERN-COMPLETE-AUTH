f377171b575fb19ed8dd82ef868d80b325065cca
Wahhaj <wahhajk9@gmail.com>
2025-07-30 22:48:30 +0500

    setup updated successfully

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import { catchAsyncError } from "../middleware/catchAsyncError.js"
 
 export const register=catchAsyncError(async(req,res,next)=>{
-    console.log("Register Route");
+    console.log("Register Route calling");
     
 })
